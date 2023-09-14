8258375c40c18f059be16c18b5225489c9f760b4
Varun Chandanala <83664525+VSVHITMAN@users.noreply.github.com>
2023-09-14 20:49:07 +0530

    Create Sets.py

---
+++ Sets.py
@@ -0,0 +1,18 @@
+# Define two sets
+E = {1, 2, 3, 4, 5}
+N = {4, 5, 6, 7, 8}
+
+print("Set E: ", E)
+print("Set N: ", N)
+
+# Union of sets
+print("Union of E and N is:", E.union(N))
+
+# Intersection of sets
+print("Intersection of E and N is:", E.intersection(N))
+
+# Difference of two sets
+print("Difference of E and N is:", E.difference(N))
+
+# Symmetric difference of two sets
+print("Symmetric difference of E and N is:", E.symmetric_difference(N))
